2fc122f85dc09c6fd6444ef5698efbc2b3b4518c
Alyssa Blanco <49414910+alyssakblanco@users.noreply.github.com>
2020-04-30 18:48:57 -0400

    submitted project

--- js/plants.js
+++ js/plants.js
@@ -54,73 +54,73 @@ btn.onclick = function(){
 one.onclick = function(){
     modal.style.display = "block";
     heading.innerText = "Ponytail Palm";
-    description.innerText = "This plant will absorb carbon dioxide and release oxygen which will help maintain a healthy environment in your room."
+    description.innerText = "The plant will absorb carbon dioxide and release oxygen which will help maintain healthy environment inyour room."
     care.innerText= "Ponytail Care:";
-    instructions.innerText="It will need a bright place in your room. The palm has to be watered once every 7-10 days when the soil looks dry.";
+    instructions.innerText="It will need bright place in your room. The palm has to be watered once in 7-10 days when the soils looks dry.";
 };
 two.onclick = function(){
     modal.style.display = "block";
     heading.innerText = "Peace Lily";
-    description.innerText = "Peace lily is a well known houseplant with dark green leaves and white blooms. This low maintenance plant will not only enhance your décor, but will also work hard to purify and clean the air in your room. Research by the American Horticultural Therapy Association found a plant in your room such as this lily can increase your memory."
+    description.innerText = "Peace lily is well known houseplant with dark green leaves and white blooms. The low maintain plant will not only will enhance your décor but also it will work hard to purify and clean air in your room. Research by the American Horticultural Therapy Association found a plant in your room such as this lily can increase your memory"
     care.innerText= "Peace Lily Care:";
-    instructions.innerText="The more sunshine the lily gets, the more it blooms. Place this plant in an area with medium to high sunlight. The soil also must be evenly moist.";
+    instructions.innerText="The more sunshine the lily will get, the more it will blooms. The soil has to be evenly moist. Place your plant in medium to bright spot.";
 };
 three.onclick = function(){
     modal.style.display = "block";
     heading.innerText = "Cacti and Succulents";
-    description.innerText = "The dryland plants are easy to take care of, and they come in different colors, shapes, and textures. The plants are perfect picks for students, since they don’t have high water needs. A study completed by the Royal College of Agriculture in England discovered that having plants in schools help students to be more attentive by 70 percent. You can have one on your desk to help you stay focused."
+    description.innerText = "The dryland plants are easy to take care for, and they come in different colors, shapes, and textures. The plants are perfect picks for students since they don’t have high water needs. A studycompleted by the Royal College of Agriculture in England discovered that having plants in schools help students to be more attentive by 70 percent. You can have one on your desk to help you stay focused."
     care.innerText= "Cacti and Succulent Care:";
-    instructions.innerText="Cacti and succulents like bright sunlight. The best place for them is a window where they can receive maximum light. Let the soil dry completely before watering.";
+    instructions.innerText="Cacti and succulents like bright sunlight. The best place is a window where they can receive maximum light. Let the soil to dry completely before each watering.";
 };
 four.onclick = function(){
     modal.style.display = "block";
     heading.innerText = "Lucky Bamboo";
-    description.innerText = "Keep a bamboo where you study. Studies by the American Horticultural Therapy Association revealed that students who have plants such as lucky bamboo in their rooms can focus better than those without."
+    description.innerText = "Grow it in medium to low light. The leaves can get sunborns if it will be under direct sunlight. The soil needs to be moistured, so grow a bamboo in water-filled pot or water it frequently."
     care.innerText= "Bamboo Care:";
-    instructions.innerText="Grown best in medium to low light. The leaves can get sunburns it's under direct sunlight. The soil needs to be moist, so grow bamboo in a water-filled pot or water it frequently.";
+    instructions.innerText="Grow it in medium to low light. The leaves can get sunborns if it will be under direct sunlight. The soil needs to be moistured, so grow a bamboo in water-filled pot or water it frequently.";
 };
 five.onclick = function(){
     modal.style.display = "block";
     heading.innerText = "Sago Palm";
-    description.innerText = "This plant has tough dark green tropical foliage. The Sago Palm is an unusual and edgy choice for a room plant.";
+    description.innerText = "It has tough dark green tropical foliage. The palm is an unusual and edgy choice dor a room plant.";
     care.innerText= "Sago Palm Care:";
-    instructions.innerText="Sago Palm loves medium to bright spots. The soil needs to be dry before being watered. It thrives under minimum care, so it will survive your busy college days. NASA research shows that having plants such as this palm in your room removes up to 87 percent of indoor pollutants.";
+    instructions.innerText="Sago palm loves medium to bright spots. The soil needs to be dry between waterings. It thrives under minimum care, so it will survive your busy college times.NASA research shows that having plants such as this palm in your room removes up to 87 percent of indoor pollutants.";
 };
 
 oneB.onclick = function(){
     modal.style.display = "block";
     heading.innerText = "Myosotis or Forget-Me-Not";
-    description.innerText = "This plant has gray-green foliage and clusters of small, light-blue flowers. The plant releases oxygen into the air that can improve your sleep quality."
+    description.innerText = "The plant has gray-green foliage and clusters of small, light-blue flowers. The plant releases oxygen into the air that can improve your sleep quality."
     care.innerText= "Forget-Me-Not Care:";
-    instructions.innerText="Requires little care. Grows best in moist, but well-drained soil. It can also grow in nutrient-poor soil, so it is not necessary to fertilize them. The pot it's in needs to have drainage holes as well. Place it in full or partial sunlit place. In winter, reduce watering to about once or twice each month, since the plant is dormant. Water it more regularly in the spring.";
+    instructions.innerText="Requires little care. Grows best in moist but well-drained soil. The pot needs to have drainage holes. It can grow in nutrient-poor soil, so it is not necessary to fertilize them. Place it in full or partial sunlight place. In winter, reduce watering to about once or twice each month, since the plant is dormant. Do normal watering in spring.";
 };
 twoB.onclick = function(){
     modal.style.display = "block";
     heading.innerText = "Aloe";
-    description.innerText = "Aloe can survive in various conditions with minimum maintenance. The plant’s extracted juice can soothe skin and heal minor burns and cuts. According to studies from the American Horticultural Therapy Association, having plants such as aloe in your room promotes a sense of optimism and well-being."
+    description.innerText = "Aloe can survive in various conditions with least maintains. The plant’s extracted juice can soothe skin and heal minor burns and cuts. According to studies from the American Horticultural Therapy Association, having plants such as aloe in your room promotes a sense of optimism and well-being"
     care.innerText= "Aloe Care:";
-    instructions.innerText="Aloe likes bright light, so keep it near a window. Its soil also needs to be evenly moist, Aloe does not like to be too dry or too moist.";
+    instructions.innerText="Aloe likes bright light, so keep it near a window. Keep the soil needs to be evenly moisturized. The plant does not like to be too dry or too watered.";
 };
 threeB.onclick = function(){
     modal.style.display = "block";
     heading.innerText = "Anthurium";
-    description.innerText = "This plant has bright and shiny red or pink blooms with deep green leaves. This beautiful plant is easy to maintain, and it will make your room feel cozy. It is tolerant for different living conditions, and if it receives enough sunlight the plant will bloom all year around. Keeping a plant in your room improves mood and promotes goal achievement, according to studies by the American Horticultural Therapy association."
+    description.innerText = "The plant has bright and shiny red or pink blooms with deep green leaves. This beautiful plant is easy to maintain, and it will make your room feel cozy. It is tolerant for different living conditions. If it receives enough sunlight, the plant will bloom all year around. Keeping a plant in your room improves mood and promotes goal achievement, according to studies by the American Horticultural Therapy association."
     care.innerText= "Anthurium Care:";
-    instructions.innerText="For best growth results, keep Arthurium in bright spot. The soil also needs to be evenly moisturized.";
+    instructions.innerText="For best growing results, keep it in bright spot. The soil needs to be evenly moisturized.";
 };
 fourB.onclick = function(){
     modal.style.display = "block";
     heading.innerText = "Snake Plant";
-    description.innerText = "The Snake Plant has vertical, striped leaves. It gives a trendy, modern look to your room! Studies by the American Horticultural Therapy Association show that caring for a plant increases levels of self-esteem."
+    description.innerText = "Snake plant has vertical, striped leaves. It gives a trendy, modern look to your room. Studies by the American Horticultural Therapy Association show that caring for a plant increases levels of self-esteem."
     care.innerText= "Snake Plant Care:";
-    instructions.innerText="They are easy to maintain. The Snake Plant only needs to be watered once a week, and it can grow in high, medium, or low sunlight. In addition, it is tolerant to dry soil, therefore it's a perfect choice for busy students!";
+    instructions.innerText="They are easy to maintain. The snake plant can grow in high, low, medium sunlight place. In addition, it istolerant to dry soil, therefore It is perfect choice for busy students. Needs to be watered once a week.";
 };
 fiveB.onclick = function(){
     modal.style.display = "block";
     heading.innerText = "Money Tree";
-    description.innerText = "Money trees are frequently grown and braided together for a more interesting look, as well as to resemble a tropical bonsai. It gives a classy, sophisticated appearance to your dorm. A research study revealed people that have plants in their rooms experience less stress and heal faster than those without.";
+    description.innerText = "Money trees are frequently grown and braided together for a more interesting look and to resemble tropical bonsai. It gives a classy, sophisticated appearance to your dorm. A research study revealed people that have plants in their rooms experience less stress and heal faster than those without.";
     care.innerText= "Money tree Care:";
-    instructions.innerText="The Money Tree needs medium to bright sunlight, and evenly moist soil.";
+    instructions.innerText="The plant needs medium to bright light and evenly moist soil.";
 };
 
 
